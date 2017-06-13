0096cd1dfd0e7352b11ecd9d0341a73bc5365d8a
zhonglun.jzl <zhonglun.jzl@alibaba-inc.com>
2017-06-13 16:39:42 +0800

    compelte first version UI

--- FilteredNamePlate.lua
+++ FilteredNamePlate.lua
@@ -1,10 +1,11 @@
 SLASH_FilteredNamePlate1 = "/fnp";
 DEBUG_LOG = true
 local GetNamePlateForUnit = C_NamePlate.GetNamePlateForUnit
+--Fnp_Mode  仅显模式 true 过滤模式 false
 
 function printInfo()
 	print("-----------")
-	print("\124cFFFF00FF插件： [ 仅显示过滤姓名板 /fnp ]\124r")
+	print("\124cFFFF00FF[过滤姓名板]\124r")
 
 	local isEnable = "不"
 	if Fnp_Enable == true then
@@ -12,21 +13,31 @@ function printInfo()
 	end
 	print("\124cFFFF00FF当前启用状态:\124r "..isEnable)	
 
-	if Fnp_Reverse ~= nil and Fnp_Reverse == true then
-		print("\124cFFFF00FF当前设置模式:\124r （仅设置的名字列表不显示）")
+	if Fnp_Mode ~= nil and Fnp_Mode == true then
+		print("\124cFFFF00FF当前设置模式:\124r 仅显模式")
 	else
-		print("\124cFFFF00FF当前设置模式:\124r （仅显示设置的名字列表）")
+		print("\124cFFFF00FF当前设置模式:\124r 过滤模式")
 	end
 
-	if Fnp_NameList ~= nil then
-		print("\124cFFFF00FF[设置的名字列表]：\124r")
+	if Fnp_FNameList ~= nil then
+		print("\124cFFFF00FF[设置的过滤列表]：\124r")
 		local i = 0
-		for key, var in ipairs(Fnp_NameList) do
+		for key, var in ipairs(Fnp_FNameList) do
 			i = i + 1
-			print(""..i..", "..var)
+			print(""..i..", ["..var.."]")
 		end
 	else
-		print("\124cFFFF00FF[设置的名字列表]\124r :空")
+		print("\124cFFFF00FF[设置的过滤列表]\124r :空")
+	end
+	if Fnp_ONameList ~= nil then
+		print("\124cFFFF00FF[设置的仅显列表]：\124r")
+		local i = 0
+		for key, var in ipairs(Fnp_ONameList) do
+			i = i + 1
+			print(""..i..", ["..var.."]")
+		end
+	else
+		print("\124cFFFF00FF[设置的仅显列表]\124r :空")
 	end
 	print("-----------")
 end
@@ -48,8 +59,8 @@ function registerMyEvents(self)
 	if Fnp_Enable == nil then
 		Fnp_Enable = false
 	end
-	if Fnp_Reverse == nil then
-		Fnp_Reverse = false
+	if Fnp_Mode == nil then
+		Fnp_Mode = false
 	end
 	
 	if IS_REGISGER == false then
@@ -71,6 +82,51 @@ function unRegisterMyEvents(self)
 	end
 end
 
+function ChangeFrameVisibility()
+	if FilteredNamePlate_Frame:IsVisible() then
+		FilteredNamePlate_Frame:Hide()
+	else
+		FilteredNamePlate_Frame:Show()
+		if Fnp_Enable == true then
+			FilteredNamePlate_Frame_EnableCheckButton:SetChecked(true);
+		else
+			FilteredNamePlate_Frame_EnableCheckButton:SetChecked(false);
+		end
+
+		if Fnp_Mode ~= nil and Fnp_Mode == true then
+			FilteredNamePlate_Frame_FilteredModeCheckBtn:SetChecked(false);
+			FilteredNamePlate_Frame_OnlyShowModeCheckBtn:SetChecked(true);
+		else
+			FilteredNamePlate_Frame_FilteredModeCheckBtn:SetChecked(true);
+			FilteredNamePlate_Frame_OnlyShowModeCheckBtn:SetChecked(false);
+		end
+		local names = ""
+		local first = true
+		for key, var in ipairs(Fnp_ONameList) do
+			if first then 
+				names = var
+				first = false
+			else
+				names = names..";"..var
+			end
+		end
+		FilteredNamePlate_Frame_OnlyShowModeEditBox:SetText(names);
+		
+		names = ""
+		first = true
+		for key, var in ipairs(Fnp_FNameList) do
+			if first then 
+				names = var
+				first = false
+			else
+				names = names..";"..var
+			end
+		end
+		FilteredNamePlate_Frame_FilteredModeEditBox:SetText(names);
+
+	end
+end
+
 function FilteredNamePlate_OnLoad(self)
 	IS_REGISGER = false
 	self:RegisterEvent("PLAYER_ENTERING_WORLD")
@@ -79,58 +135,66 @@ function FilteredNamePlate_OnLoad(self)
 	-- self:RegisterEvent("CHALLENGE_MODE_RESET")
 end
 
+function ModeCheckButtonChecked(mode, checked)
+	if mode == "o" then
+		if checked == true then
+			Fnp_Mode = true
+			FilteredNamePlate_Frame_FilteredModeCheckBtn:SetChecked(false);
+		else
+			Fnp_Mode = false
+			FilteredNamePlate_Frame_FilteredModeCheckBtn:SetChecked(true);
+		end
+	else
+		if checked == true then
+			Fnp_Mode = false
+			FilteredNamePlate_Frame_OnlyShowModeCheckBtn:SetChecked(false);
+		else
+			Fnp_Mode = true
+			FilteredNamePlate_Frame_OnlyShowModeCheckBtn:SetChecked(true);
+		end
+	end
+	printInfo("a")
+end
 
+function EnableButtonChecked(self, checked)
+	if (checked) then
+		Fnp_Enable = true;
+		registerMyEvents(self)
+	else
+		Fnp_Enable = false;
+		unRegisterMyEvents(self)
+	end
+	printInfo("a")
+end
+
+function ModeEditBoxWriten(mode, inputStr)
+	if mode == "o" then
+		Fnp_ONameList = {}  
+		string.gsub(inputStr, '[^;]+', function(w) table.insert(Fnp_ONameList, w) end )
+	else
+		Fnp_FNameList = {}  
+		string.gsub(inputStr, '[^;]+', function(w) table.insert(Fnp_FNameList, w) end )
+	end
+	printInfo("a")
+end
 
 function SlashCmdList.FilteredNamePlate(msg)
 	local lastTarget = GetBindingKey("TARGETNEARESTENEMY");
 	if msg == "" then
-		print("------")
-		print("\124cFFFF00FF/fnp enable1\124r  开启功能（仅显示设置的名字列表）")
-		print("\124cFFFF00FF/fnp enable2\124r  开启功能（仅设置的名字列表不显示）")
-		print("\124cFFFF00FF/fnp disable\124r  关闭功能")
-		print("\124cFFFF00FF/fnp names=AAA;BBB;CCC\124r     请注意等号前后没有空格;分号是英文符号;不断的追加分号和额外的名字即可")
-		print("\124cFFFF00FF/fnp clearns\124r  清理名字列表")
-		print("------")
-		printInfo("a")
-	elseif msg == "enable1" then
-		Fnp_Enable = true
-		Fnp_Reverse = false
-		printInfo()
-		print("\124cFFFF00FF变动生效，需要按下关闭和显示敌对快捷键即可出现！\124r")
-	elseif msg == "enable2" then
-		Fnp_Enable = true
-		Fnp_Reverse = true
-		printInfo()
-		print("\124cFFFF00FF变动生效，需要按下关闭和显示敌对快捷键即可出现！\124r")
-	elseif msg == "disable" then
-		Fnp_Enable = false
-		printInfo()
-		print("\124cFFFF00FF变动生效，需要按下关闭和显示敌对快捷键即可出现！\124r")
-	elseif msg == "clearns" then
-		Fnp_NameList = {}
-		printInfo("a")
-		print("\124cFFFF00FF变动生效，需要按下关闭和显示敌对快捷键即可出现！\124r")
-	else
-		local _, num = string.find(msg, 'names=')
-		if num == 6 then
-			local str = string.gsub(msg, "names=", "") 
-			Fnp_NameList = {}  
-			string.gsub(str, '[^;]+', function(w) table.insert(Fnp_NameList, w) end )
-		end
-		printInfo("a")
-		print(" ")
-		print("\124cFFFF00FF变动生效，需要按下关闭和显示敌对快捷键即可出现！\124r")
-		print("如果不正确，请重新设置。实在不行，可以/fnp clearns 清理。")
+		print("\124cFFFF00FF[过滤姓名板]\124r")
+		print("/fnp options 或者/fnp opt 打开菜单")
+	elseif msg == "options" or msg == "opt" then
+		ChangeFrameVisibility()
 	end
 end
 
-function isMatchMyNameList(tName)
-	if Fnp_Enable == false then
+function isMatchFilteredNameList(tName)
+	if Fnp_Enable == false or Fnp_Mode == true then
 		return false
 	end
 
 	local isMatch = false
-	for key, var in ipairs(Fnp_NameList) do
+	for key, var in ipairs(Fnp_FNameList) do
 		local _, ret = string.find(tName, var)
 		if ret ~= nil then
 			isMatch = true
@@ -140,42 +204,70 @@ function isMatchMyNameList(tName)
 	return isMatch
 end
 
+function isMatchOnlyShowNameList(tName)
+	if Fnp_Enable == false or Fnp_Mode == false then
+		return false
+	end
+
+	local isMatch = false
+	for key, var in ipairs(Fnp_ONameList) do
+		local _, ret = string.find(tName, var)
+		if ret ~= nil then
+			isMatch = true
+			break
+		end
+	end
+	return isMatch
+end
+
+function insertBuffValue(tab, value)
+    local isExist = false;
+    for pos, name in ipairs(tab) do
+        if (name == value) then        
+            isExist = true;
+        end
+    end
+    if not isExist then table.insert(tab, value) end;
+end
+
+function removeMyTabUnits(tab, value)
+    for pos, name in ipairs(tab) do
+        if (name == value) then        
+            table.remove(tab, pos)         
+        end
+    end
+end
+
 function FilteredNamePlate_OnEvent(self,event,...)
-	if event == "CHALLENGE_MODE_COMPLETED" then
-		Fnp_Enable = false
-		unRegisterMyEvents(self)
-		printInfo()
-	elseif event == "CHALLENGE_MODE_START" or event == "CHALLENGE_MODE_RESET" then
-		Fnp_Enable = true
-		printInfo()
-	elseif event == "PLAYER_ENTERING_WORLD" then
-		printInfo()
-		registerMyEvents(self)
-	elseif event == "NAME_PLATE_UNIT_ADDED" then  -- Added
+	if event == "NAME_PLATE_UNIT_ADDED" then  -- Added
+		if Fnp_Enable == false then
+			return
+		end
+		if Fnp_Mode == true then
+			local count = table.getn(Fnp_ONameList)
+			if count == 0 then return end
+		end
 		local unitid = ...
-		-- Personal Display
 		local name = UnitName(unitid)
-		-- printLog("NAME_PLATE_UNIT_ADDED"..name)
-		local matched = isMatchMyNameList(name)
-		if Fnp_Reverse ~= nil and Fnp_Reverse == true then -- 都显示，除了不显示列表中
-			-- printLog("aaaaaaaa")
-			if matched == true then
-				-- printLog("bbbbbbbb")
+		local matched = false
+		-- 仅显模式
+		if Fnp_Mode == true then
+			matched = isMatchOnlyShowNameList(name)
+			if matched == false then
 				local plate = GetNamePlateForUnit(unitid);
 				plate:GetChildren():Hide()
 			end
-		else  -- 默认模式，只显示过滤名单
-			-- printLog("ccccc")
-			if matched == false then
-				-- printLog("ddddd")
+		-- 过滤模式
+		else
+			matched = isMatchFilteredNameList(name)
+			if matched == true then
 				local plate = GetNamePlateForUnit(unitid);
 				plate:GetChildren():Hide()
 			end
 		end
-
 	elseif event == "NAME_PLATE_UNIT_REMOVED" then  -- Removed
-		--printLog("NAME_PLATE_UNIT_REMOVED")
 	elseif event == "NAME_PLATE_CREATED" then  -- Created
-		--printLog("NAME_PLATE_CREATED")
+	elseif event == "PLAYER_ENTERING_WORLD" then
+		registerMyEvents(self)
 	end
 end
